baf4921d81b47fa12ba66e2762b5b0d403bd36e5
F0lak <f0lak.haz@gmail.com>
2026-06-09 23:09:06 -0600

    updates readme and addresses tests failing on runner

--- ref_splitter/dm_ref.py
+++ ref_splitter/dm_ref.py
@@ -12,6 +12,9 @@ class DMRef:
     '''Grabs and stores info.html from the byond webserver'''
 
     dm_ref_url: str = "https://www.byond.com/docs/ref/info.html"
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+    }
     timeout_seconds: int = 10
     timeout_error_message = "Timeout exceeds maximum value (10 seconds)"
 
@@ -28,7 +31,7 @@ class DMRef:
         Raises:
             requests.RequestException if the network request fails or times out
         '''
-        response = requests.get(self.dm_ref_url, timeout=self.timeout_seconds)
+        response = requests.get(self.dm_ref_url, headers=self.headers, timeout=self.timeout_seconds)
         response.raise_for_status()
         self.ref_info = response.text
         return self.ref_info
